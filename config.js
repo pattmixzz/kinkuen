3307ed7ea5d9bced6c5f801186b2d75b3b579884
pattmixzz <pattmixzz@gmail.com>
2026-09-25 15:11:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // วาง Web app URL จาก Apps Script (Deploy → Manage deployments) ตรงนี้
 window.APP_CONFIG = {
-  API_URL: 'https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnQ9It00NnxxFd-o3onAxKS45vyF3kI4arbjqsBNK8ZMrh99OPdovevTY6xYxfCkZcTMiNQXaZ_iXbJVtm92Km6td9SJoTQ8N76cD3gIwvrWvAPCRbRuc37683nNyuTdNzDkgerQ4ox5Yg2_JMG4iOiUI6XIb8oVRf0jvGCaYK7I-tZjkeQJvXPRWV0kL4RaRLMB76qMrjXutNJlF-WOpb5rah6OtBulOGoGHoPFCjZRS6mGzviMgWcGWOeffMKxXzPBn6zL6PMSxYJwvdZhAKqXX9nimw&lib=MbhnUP-z_LB5ecUTPTCafARuo7Szv-4Yx'
+  API_URL: 'https://script.google.com/macros/s/AKfycbzsUcohtPtVkHaHZF-B1V03QhsE8AYC3PA30k4rzpCS2Rh4A3IUuCxWi6qElY0kErOEoA/exec'
 };
